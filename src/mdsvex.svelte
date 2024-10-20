aa840bbd5dbbbd86ad9b139b1c9d9a0ba960e2b8
kushal <kushalgaikwad140@gmail.com>
2024-10-21 01:26:41 +0530

    Add project

---
+++ src/mdsvex.svelte
@@ -0,0 +1,6 @@
+<script lang="ts" context="module">
+	import  img  from '$lib/components/custom/img.svelte'
+	export { img }
+</script>
+
+<slot />
---
+++ svelte.config.js
@@ -0,0 +1,42 @@
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { getHighlighter } from 'shiki'
+import remarkUnwrapImages from 'rehype-unwrap-images'
+import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+	rehypePlugins: [rehypeSlug]
+}
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	kit: {
+		adapter: adapter(),
+		alias: { 'lib/': './src/routes/lib/' },
+		alias: { 'lib/': './src/routes/lib/utils/' }
+		
+	}
+}
+
+export default config
